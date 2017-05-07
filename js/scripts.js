cce16a5163be5a5cd19fb897f56891c1c5c188a7
Simpo <marcusgathii2011@gmail.com>
2017-05-07 11:14:26 +0300

    Game

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// var lives =3;
+//
+// var example = "I am a variable "
+//
+// //document.write(example);
+//
+// example = "This is another example for the same variable";
+//
+// //document.write(example);
+alert("Welcome!!!");
+
+var VisitorAge = prompt ("What is Your favorite Colour? ");
+
+var VisitorName = prompt ("What is Your name? ");
+
+document.write ("Hello " + VisitorName + " Your favorite Colour is " + VisitorAge);
+
+console.log("Program Complete!");
